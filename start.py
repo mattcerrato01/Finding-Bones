09f2a8e8d0e271145564a6d66bfbc15d065a96b0
nenglish2 <nickenglish@gmail.com>
2020-01-28 15:21:38 -0500

    start screen

--- start.py
+++ start.py
@@ -2,6 +2,7 @@
 import pygame as p
 import Objects
 import GameStates as gs
+import credits
 
 p.init()
 
@@ -16,19 +17,23 @@ def main(screen):
 	background = p.Surface(screen.get_size())
 	background = background.convert()
 	background.fill((250, 250, 250))
-	startbutton = loadify("villager_front_m.png")
+	startbutton = loadify("Bones-WalkingRight-Rightfoot.png")
 	startbutton = p.transform.scale(startbutton, (100,80))
 	startbuttonpos = startbutton.get_rect()
 	#print(startbuttonpos)
 	startbuttonpos.centerx = background.get_rect().centerx
-	startbuttonpos = startbuttonpos.move(0,300)
+	startbuttonpos = startbuttonpos.move(0,350)
 	#print(startbuttonpos)
 	
 	font = p.font.Font(None, 36)
-	text = font.render("Click Here to Start", 1, (10, 10, 10))
+	text = font.render("", 1, (10, 10, 10))
 	textpos = text.get_rect()
 	textpos.centerx = background.get_rect().centerx
 	textpos = textpos.move(0,150)
+	back = loadify("startscreen.png")
+	backpos = back.get_rect()
+	back = p.transform.scale(back, (800,600))
+	background.blit(back, backpos)
 	background.blit(text, textpos)
 	background.blit(startbutton, startbuttonpos)
 	screen.blit(background, (0, 0))
@@ -39,7 +44,7 @@ def main(screen):
 	while running:
 		#screen.fill([255, 255, 255])
 		clicked = False
-		#print(p.mouse.get_pos())
+		print(p.mouse.get_pos())
 		for event in p.event.get():
 			if event.type == p.QUIT:
 				running = False
@@ -49,12 +54,18 @@ def main(screen):
 		if key[p.K_ESCAPE]:
 			running = False
 		
-		if 300 < p.mouse.get_pos()[0] < 500 and 140 < p.mouse.get_pos()[1] < 180:
-			#print("mouse is over 'newGameButton'")
-			if clicked:
-				#gs.change_track(1)
+		if clicked:
+			if 90 < p.mouse.get_pos()[0] < 350 and 445 < p.mouse.get_pos()[1] < 530:
 				running = False
+			elif 480 < p.mouse.get_pos()[0] < 730 and 445 < p.mouse.get_pos()[1] < 530:
+				c = credits.main(screen)
+				if c == "quit":
+					endrunning = False
+					return "quit"
+				if c == "back":
+					endrunning = True
 		background.fill((250, 250, 250))
+		background.blit(back, backpos)
 		background.blit(text, textpos)
 		background.blit(startbutton, startbuttonpos)
 		p.mouse.set_visible(False)
