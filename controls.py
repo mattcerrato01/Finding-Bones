76ffc2fba36f28bb0f8e1b44d7f4e22ca3cf61db
mattcerrato <mattcerrato01@gmail.com>
2020-01-30 11:06:48 -0500

    Controls screen

--- controls.py
+++ controls.py
@@ -11,15 +11,20 @@ def play_sound(name):
 	sound.play()
 
 def main(screen):
+	background = p.Surface(screen.get_size())
+	background = background.convert()
+	background.fill((250, 250, 250))
 
 	back = loadify('controlscreen.png')
 	backpos = back.get_rect()
 	back = p.transform.scale(back, (800,600))
 	backbutton = p.rect.Rect(615, 520, 165, 70)
-	back_text = p.font.SysFont("Times New Roman", 20)
+	back_text = p.font.SysFont("Times New Roman", 40)
 	back_texts = back_text.render("Back", True, (255,255,255))
 	text_rect = back_texts.get_rect()
-	screen.blit(back, (0,0))
+	background.blit(back, backpos)
+	background.blit(back_texts, (backbutton.x + backbutton.width/2 - text_rect.width/2, backbutton.y + backbutton.height/2 - text_rect.height/2 ))
+	screen.blit(background, (0, 0))
 	p.display.flip()
 	running = True
 	cursor = p.transform.scale(Objects.loadify("cursor-small-arrow.png").convert_alpha(), (15,15))
@@ -39,14 +44,16 @@ def main(screen):
 			running = False
 		if backbutton.collidepoint(p.mouse.get_pos()) and clicked:
 			return "back"
-		screen.blit(back, backpos)
+		background.fill((250, 250, 250))
+		background.blit(back, backpos)
 		p.mouse.set_visible(False)
-		p.draw.rect(screen, (0,0,0),backbutton)
-		screen.blit(back_texts, (backbutton.x + backbutton.width/2 - text_rect.width/2, backbutton.y + backbutton.height/2 - text_rect.height/2 ))
-		screen.blit(cursor, p.mouse.get_pos())
+		p.draw.rect(background, (0,0,0),backbutton)
+		background.blit(back_texts, (backbutton.x + backbutton.width/2 - text_rect.width/2, backbutton.y + backbutton.height/2 - text_rect.height/2 ))
+		background.blit(cursor, p.mouse.get_pos())
+		screen.blit(background, (0,0))
 
 
-	p.display.flip()
+		p.display.flip()
 
 if __name__ == "__main__":
 	main(p.display.set_mode((800, 600)))
